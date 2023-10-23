3d7b827f19eef6081ae49631ff3f02618a06fda5
jamartinezv2020 <jamartinezv2020@gmail.com>
2023-10-22 23:51:49 -0500

    actualización de la estructura de clases para el ejercicio Facturaciom3

--- src/main/java/org/example/ejercicios/Facturaciom3.java
+++ src/main/java/org/example/ejercicios/Facturaciom3.java
@@ -1,31 +1,50 @@
 package org.example.ejercicios;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 public class Facturaciom3 {
-	record Factura(String cliente, String fechafactura, List<Double> precioProducto) {
+	record Factura(String numeroFactura, String cliente, LocalDate fechaFactura, List<Double> precioProducto) {
 	}
 
 	public static void main(String[] args) {
-		Factura factura1 = new Factura("Maria Sanabria", "Julio 1, 1999", List.of(4500D, 5000D, 2500D));
-		Factura factura2 = new Factura("Juan Perez", "Agosto 15, 2000", List.of(3500.0, 6000D, 2800D));
+		Factura factura1 = new Factura("1","Maria Sanabria", LocalDate.of(1999, Month.JULY, 1), List.of(4500.0D, 5000.0D, 2500.0D));
+		Factura factura2 = new Factura("2","Juan Pérez", LocalDate.of(2000, Month.AUGUST, 15), List.of(3500.0D, 6000.0D, 2800.0D));
+		Factura factura3 = new Factura("3","José Alfredo Martínez Valdés", LocalDate.of(2023, Month.OCTOBER, 23), List.of(10500.0D, 16000.0D, 12800.0D));
+		new MostraFacturacion (factura1);
+		new MostraFacturacion (factura2);
+		new MostraFacturacion (factura3);
 
-		double totalValorFact1 = 0;
-		for (Double precio : factura1.precioProducto) {
-			totalValorFact1 += precio;
-		}
-		System.out.println("Factura 1" );
-		System.out.println("Cliente: " + factura1.cliente());
-		System.out.println("Total: " + totalValorFact1);
-
-
-		double totalValorFact2 = 0;
-		for (Double precio : factura2.precioProducto) {
-			totalValorFact2 += precio;
-		}
-		System.out.println("Factura 2" );
-		System.out.println("Cliente: " + factura2.cliente());
-		System.out.println("Total: " + totalValorFact2);
 	}
+
+		static class CalculadoraFactura {
+
+		public double calcularTotal(Factura factura) {
+			double totalValorFactura = 0.0D;
+			for (Double precio : factura.precioProducto ()) {
+				totalValorFactura += precio;
+			}
+			return totalValorFactura;
+		}
+	}
+
+
+	static class MostraFacturacion{
+
+		public MostraFacturacion (Factura factura) {
+			CalculadoraFactura calculadoraFactura = new CalculadoraFactura ();
+			System.out.println(" " );
+			System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" );
+			System.out.println("Factura " + factura.numeroFactura ());
+			System.out.println("Fecha: " + factura.fechaFactura);
+			System.out.println("Cliente: " + factura.cliente());
+			System.out.println("Total: " + calculadoraFactura.calcularTotal (factura));
+			System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" );
+			
+		}
+
+	}
+
 
 }
